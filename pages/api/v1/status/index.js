2e24267997dc0293d7132c3ac4f52ce47d582269
cramancio <cramancio.silva@gmail.com>
2024-09-23 13:55:43 -0300

    finaliza o `endpoint /status `

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,34 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const resultado = await database.query("SELECT 1 + 1 as sum;");
-  response.status(200).json(`Teste de  criação de api!`);
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  //server_version > propiedade que  nos inform a versão do banco postgres usado no projeto.
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsValue);
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 export default status;
